5f052a9000062c35c078ae3103656c5765f3a206
pvanderlaat <pvanderlaat@gmail.com>
2022-11-15 10:07:38 -0500

    pause screen and fixed fish animation

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject pauseButton;
+
+    public void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            IsPressed();
+        }
+    }
+    public void IsPressed()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        pauseMenuUI.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        // SceneManager.LoadScene("Main Menu Scene");
+        Debug.Log("Quit Game");
+    }
+}
