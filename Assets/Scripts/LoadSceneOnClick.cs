711917de6f31d8c0aee1ffc0b5579f62c471849c
fernandezmatthew <fernandezmatthew@ufl.edu>
2022-12-05 15:42:25 -0500

    Started making level ui buttons

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour {
+    Scene scene;
 
+    public void Start() {
+        
+    }
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void RestartLevel() {
+        scene = SceneManager.GetActiveScene();
+        LoadByIndex(scene.buildIndex);
     }
 }
